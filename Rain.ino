fc5d16b6d9b22ece4a0a712172f35f8eba1a1af3
Jomiel <enriquez.jliquigan@gmail.com>
2023-05-13 09:04:39 +0800

    with timer

--- Rain.ino
+++ Rain.ino
@@ -10,8 +10,9 @@ const int RAIN_OUT = 2;
 const int SOLAR_PIN = A1;
 const int DELAY = 500;
 
-const int STOP_FORWARD = 8;
-const int STOP_REVERSE = 10;
+const int MOTORDELAY = 5000;  // delay on motor to stop
+// const int STOP_FORWARD = 8;
+// const int STOP_REVERSE = 7;
 
 // Threshold values
 const int RAIN_THRESHOLD = 500; // Change this value to adjust rain sensitivity
@@ -22,38 +23,39 @@ int rainOUT = 0;
 int prevRainOUT = rainOUT;
 
 bool isNewValue = false;
-bool isStopForward = false;
-bool isStopReverse = false;
+// bool isStopForward = false;
+// bool isStopReverse = false;
 
-int buttonStateStopForward = 0;
-int buttonStateStopReverse = 0;
+// int buttonStateStopForward = 0;
+// int buttonStateStopReverse = 0;
 
 void setup() {
   // Set pin modes
   pinMode(RELAY1_PIN, OUTPUT);
   pinMode(RELAY2_PIN, OUTPUT);
-  pinMode(STOP_FORWARD, INPUT);
-  pinMode(STOP_REVERSE, INPUT);
+  // pinMode(STOP_FORWARD, INPUT);
+  // pinMode(STOP_REVERSE, INPUT);
   
   //Serial 
   digitalWrite(RELAY1_PIN, HIGH);
   digitalWrite(RELAY2_PIN, HIGH);
+
   delay(1000);
   Serial.begin(9600);
 }
 
 void loop() {
-  buttonStateStopForward =  digitalRead(STOP_FORWARD);
-  buttonStateStopReverse =  digitalRead(STOP_REVERSE);
-  if(buttonStateStopForward == 0){
-    Serial.print("F pressed ");
-    isStopForward = true;
-  }
+  // buttonStateStopForward =  digitalRead(STOP_FORWARD);
+  // buttonStateStopReverse =  digitalRead(STOP_REVERSE);
+  // if(buttonStateStopForward == 0){
+  //   Serial.print("F pressed ");
+  //   isStopForward = true;
+  // }
 
-  if(buttonStateStopReverse == 0){
-    Serial.print("R pressed ");
-    isStopReverse = true;
-  }
+  // if(buttonStateStopReverse == 0){
+  //   Serial.print("R pressed ");
+  //   isStopReverse = true;
+  // }
   // Read the rain sensor value
   rainLevel = analogRead(RAIN_PIN);
   // value becomes zero when reach the treshold
@@ -65,10 +67,10 @@ void loop() {
   else{
     isNewValue = false;
   }
-  Serial.print("FB Stop: ");
-  Serial.print(buttonStateStopForward);
-  Serial.print(" RB Stop: ");
-  Serial.print(buttonStateStopReverse);
+  // Serial.print("FB Stop: ");
+  // Serial.print(buttonStateStopForward);
+  // Serial.print(" RB Stop: ");
+  // Serial.print(buttonStateStopReverse);
   Serial.print(" Rain Level: ");
   Serial.print(rainLevel);
   Serial.print(" Rain output: ");
@@ -78,15 +80,13 @@ void loop() {
     ResetMotor();
   }
   // Check the value of rainOUT
-  if(rainOUT==0 && !isStopForward){
-    isStopReverse = false;
-    Serial.print(" MOTOR FORWARD");
-    Forward();
+  if(rainOUT==0){
+    if(isNewValue){
+      Forward();
+    }
   }
   else{
-    if(!isStopReverse){
-      isStopForward = false;
-    Serial.print(" MOTOR REVERSE");
+    if(isNewValue){
       Reverse();
     }
   }
@@ -103,11 +103,15 @@ void ResetMotor(){
 void Forward(){
   digitalWrite(RELAY1_PIN, HIGH);
   digitalWrite(RELAY2_PIN, LOW);
+  delay(MOTORDELAY);
+  ResetMotor();
 }
 
 void Reverse(){
   digitalWrite(RELAY1_PIN, LOW);
   digitalWrite(RELAY2_PIN, HIGH);
+  delay(MOTORDELAY);
+  ResetMotor();
 }
 
 void ForwardReverse(){
